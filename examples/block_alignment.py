386aec259f3979d49bc58055a7f150d30152a680
David Meijer <david-meijer@live.nl>
2026-01-05 16:16:12 +0100

    ENH: add docking functionalities

--- examples/block_alignment.py
+++ examples/block_alignment.py
@@ -4,7 +4,7 @@
 
 from versalign.scoring import create_substituion_matrix_dynamically
 from versalign.aligner import setup_aligner
-from versalign.blocks import calc_block_msa
+from versalign.blocking import calc_block_msa
 
 
 def main() -> None:
@@ -28,17 +28,18 @@ def main() -> None:
     d_blocks = [list("AAAACCT")]
     e_blocks = [list("AAAACGT"), list("ACGGGA"),  list("T"), ]
     f_blocks = [list("T"), list("AGG"), list("GCA"), list("TGCAAAA")]
+    g_blocks = [list("T")]
 
     msa = calc_block_msa(
         aligner,
-        [a_blocks, b_blocks, c_blocks, d_blocks, e_blocks, f_blocks],
+        [a_blocks, b_blocks, c_blocks, d_blocks, e_blocks, f_blocks, g_blocks],
         gap_repr='-',
         allow_block_reverse=True
     )
     
     print("\nBlock MSA example:")
     for row, label in zip(msa.msa, msa.order):
-        print(label[0], "".join(["".join(block) for block in row]), sep="\t")
+        print(label[0], "|".join(["".join(block) for block in row]), sep="\t")
 
 
 if __name__ == "__main__":
